d64a688d51e0bc4852ca93948a8faf06288efd12
TomMonks <thomas.m.monks@gmail.com>
2024-03-01 15:36:41 +0000

    OVERV: updated code to read bkg txt from URL

--- Overview.py
+++ Overview.py
@@ -1,35 +1,7 @@
 import streamlit as st
 from PIL import Image
 import urllib
-
-INFO_1 = '''**A simple simulation model of a urgent care and treatment centre.**'''
-OVERVIEW_PATH = 'txt/overview.md'
-
-@st.experimental_singleton(show_spinner=True)
-def get_file_content_as_string(path):
-    '''
-    Download the content of a file from the GitHub Repo and return as a utf-8 string
-
-    Notes:
-    -------
-        temporarily this is pointing at the `dev` branch
-
-        adapted from 'https://github.com/streamlit/demo-self-driving'
-
-    Parameters:
-    ----------
-    path: str
-        e.g. file_name.md
-
-    Returns:
-    --------
-    utf-8 str
-
-    '''
-    url = 'https://raw.githubusercontent.com/TomMonks/treat_sim_streamlit/dev/' + path
-    response = urllib.request.urlopen(url)
-    return response.read().decode("utf-8")
-
+import urllib.request as request
 
 st.set_page_config(
      #page_title="Ex-stream-ly Cool App",
@@ -43,6 +15,63 @@ st.set_page_config(
      }
  )
 
+INFO_1 = '''**A simple simulation model of a urgent care and treatment centre.**'''
+
+OVERVIEW_PATH = (
+    "https://raw.githubusercontent.com/pythonhealthdatascience/"
+    + "stars-streamlit-example/main/txt/overview.md"
+)
+
+
+def read_file_contents(path):
+    """
+    Download the content of a file from the GitHub Repo and return as a utf-8 string
+
+    Notes:
+    -------
+        adapted from 'https://github.com/streamlit/demo-self-driving'
+
+    Parameters:
+    ----------
+    path: str
+        e.g. file_name.md
+
+    Returns:
+    --------
+    utf-8 str
+
+    """
+    response = request.urlopen(path)
+    return response.read().decode("utf-8")
+
+
+
+
+# @st.experimental_singleton(show_spinner=True)
+# def get_file_content_as_string(path):
+#     '''
+#     Download the content of a file from the GitHub Repo and return as a utf-8 string
+
+#     Notes:
+#     -------
+#         temporarily this is pointing at the `dev` branch
+
+#         adapted from 'https://github.com/streamlit/demo-self-driving'
+
+#     Parameters:
+#     ----------
+#     path: str
+#         e.g. file_name.md
+
+#     Returns:
+#     --------
+#     utf-8 str
+
+#     '''
+#     url = 'https://raw.githubusercontent.com/TomMonks/treat_sim_streamlit/dev/' + path
+#     response = urllib.request.urlopen(url)
+#     return response.read().decode("utf-8")
+
 st.title('Treatment Centre Simulation Model')
 
 image = Image.open('img/nihr.png')
@@ -51,4 +80,4 @@ st.image(image)
 st.markdown(INFO_1)
 
 #plain english summary
-st.markdown(get_file_content_as_string(OVERVIEW_PATH))
+st.markdown(read_file_contents(OVERVIEW_PATH))
