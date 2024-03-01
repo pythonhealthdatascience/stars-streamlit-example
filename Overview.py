c12aa1dc0f86e54ccdc45767176ad9ff4111addd
TomMonks <thomas.m.monks@gmail.com>
2024-03-01 15:43:19 +0000

    PAGES: updated read text ABOUT

--- Overview.py
+++ Overview.py
@@ -4,18 +4,18 @@ import urllib
 import urllib.request as request
 
 st.set_page_config(
-     #page_title="Ex-stream-ly Cool App",
-     #page_icon="🧊",
-     layout="wide",
-     initial_sidebar_state="expanded",
-     menu_items={
-     #    'Get Help': 'https://www.extremelycoolapp.com/help',
-     #    'Report a bug': "https://www.extremelycoolapp.com/bug",
-         'About': "## Treatment centre sim.  Adapted from Nelson (2013)."
-     }
- )
+    # page_title="Ex-stream-ly Cool App",
+    # page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        #    'Get Help': 'https://www.extremelycoolapp.com/help',
+        #    'Report a bug': "https://www.extremelycoolapp.com/bug",
+        "About": "## Treatment centre sim.  Adapted from Nelson (2013)."
+    },
+)
 
-INFO_1 = '''**A simple simulation model of a urgent care and treatment centre.**'''
+INFO_1 = """**A simple simulation model of a urgent care and treatment centre.**"""
 
 OVERVIEW_PATH = (
     "https://raw.githubusercontent.com/pythonhealthdatascience/"
@@ -45,39 +45,12 @@ def read_file_contents(path):
     return response.read().decode("utf-8")
 
 
+st.title("Treatment Centre Simulation Model")
 
-
-# @st.experimental_singleton(show_spinner=True)
-# def get_file_content_as_string(path):
-#     '''
-#     Download the content of a file from the GitHub Repo and return as a utf-8 string
-
-#     Notes:
-#     -------
-#         temporarily this is pointing at the `dev` branch
-
-#         adapted from 'https://github.com/streamlit/demo-self-driving'
-
-#     Parameters:
-#     ----------
-#     path: str
-#         e.g. file_name.md
-
-#     Returns:
-#     --------
-#     utf-8 str
-
-#     '''
-#     url = 'https://raw.githubusercontent.com/TomMonks/treat_sim_streamlit/dev/' + path
-#     response = urllib.request.urlopen(url)
-#     return response.read().decode("utf-8")
-
-st.title('Treatment Centre Simulation Model')
-
-image = Image.open('img/nihr.png')
+image = Image.open("img/nihr.png")
 st.image(image)
 
 st.markdown(INFO_1)
 
-#plain english summary
+# plain english summary
 st.markdown(read_file_contents(OVERVIEW_PATH))
