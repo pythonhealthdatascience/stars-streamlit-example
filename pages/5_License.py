dce9378ec425ab19b2f54b6f13c5c10e7a73aefa
TomMonks <thomas.m.monks@gmail.com>
2024-04-20 13:40:34 +0100

    LICENSE: license web app page

---
+++ pages/5_License.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import urllib.request as request
+
+LICENSE_FILE = (
+    "https://raw.githubusercontent.com/pythonhealthdatascience/"
+    + "stars-streamlit-example/dev/LICENSE"
+)
+
+LICENSE_PAGE_TXT = (
+    "https://raw.githubusercontent.com/pythonhealthdatascience/"
+    + "stars-streamlit-example/dev/txt/license_page.md"
+)
+
+def read_file_contents(path):
+    """
+    Download the content of a file from the GitHub Repo and return as a utf-8 string
+
+    Notes:
+    -------
+        adapted from 'https://github.com/streamlit/demo-self-driving'
+
+    Parameters:
+    ----------
+    path: str
+        e.g. file_name.md
+
+    Returns:
+    --------
+    utf-8 str
+
+    """
+    response = request.urlopen(path)
+    return response.read().decode("utf-8")
+
+# show license header and badge
+st.markdown(read_file_contents(LICENSE_PAGE_TXT))
+
+# show MIT license.
+license_txt = read_file_contents(LICENSE_FILE)
+st.code(license_txt, language="markdown", line_numbers=False)
