7c6c0a52e9bf4958a9bb6d1a4f6225b0fe3dda77
amyheather <a.heather2@exeter.ac.uk>
2024-06-13 09:59:22 +0100

    Interactive sim: PEP-8, consistent " ', and help statements for each widget

--- pages/0_🎱_Interactive_simulation.py
+++ pages/0_🎱_Interactive_simulation.py
@@ -1,11 +1,6 @@
-from contextlib import suppress
 import streamlit as st
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
 from PIL import Image
 from treat_sim import model as md
-import plotly.express as px
 from more_plot import more_plotly
 from scripts.arrival_chart import get_arrival_chart
 from scripts.setup import page_config
@@ -13,95 +8,101 @@ from scripts.setup import page_config
 # Set page config
 page_config()
 
-INFO_1 = '''**A simple simulation model of a urgent care and treatment centre.**'''
-INFO_1a = '''**Change the model parameters and rerun to see the effect on waiting times and 
-utilisation of rooms**'''
+INFO_1 = "**A simple simulation model of a urgent care and treatment centre.**"
+INFO_2 = """**Change the model parameters and rerun to see the effect on
+waiting times and utilisation of rooms**"""
 
-INFO_2 = '**Run 5 pre-specified scenarios and compare results.**'
+INFO_3 = """**Trauma arrivals:**
+patients with severe illness and trauma that must first be stablised in a
+trauma room. These patients then undergo treatment in a cubicle before being
+discharged."""
 
-INFO_3 = '''**Trauma arrivals:**
-patients with severe illness and trauma that must first be stablised in a 
-trauma room. These patients then undergo treatment in a cubicle before being 
-discharged.'''
-
-INFO_4 = '''**Non-trauma arrivals**
-patients with minor illness and no trauma go through registration and 
+INFO_4 = """**Non-trauma arrivals**
+patients with minor illness and no trauma go through registration and
 examination activities. A proportion of non-trauma patients require treatment
-in a cubicle before being discharged. '''
+in a cubicle before being discharged."""
 
-SC_TABLE = '''
-|   | Scenario                | Description                                                          |
-|---|-------------------------|----------------------------------------------------------------------|
-| 1 | As-is                   | Uses default settings - represents how the system currently operates |
-| 2 | Triage + 1              | Add an additional triage bay for new patients                        |
-| 3 | Exam + 1                | Add an additional examination cubicle for the non-trauma pathway     |
-| 4 | Treat + 1               | Add an extra non-trauma treatment cubicle                            |
-| 5 | Swap Exam & Treat       | Convert an exam room into a non_trauma treatment cubicle             |
-| 6 | Scenario 5 + short exam | Scenario 5 changes + examination takes 4 mins less on average        |
+# Title
+st.title("Treatment Centre Simulation Model")
 
-'''
-
-st.title('Treatment Centre Simulation Model')
-
-image = Image.open('img/nihr.png')
+# NIHR logo
+image = Image.open("img/nihr.png")
 st.image(image)
 
+# Intro sentence
 st.markdown(INFO_1)
 
-# Using "with" notation
+# Create widgets to adjust model parameters within the sidebar
 with st.sidebar:
-    st.markdown('# Parameters')
-    
-    st.markdown('## Capacity constraints')
-   
-    triage_bays = st.slider('Triage bays', 1, 5, md.DEFAULT_N_TRIAGE)
-    exam_rooms = st.slider('Exam rooms', 1, 5, md.DEFAULT_N_EXAM)
-    treat_rooms = st.slider('Non-Trauma Treatment cubicles', 1, 5, md.DEFAULT_N_CUBICLES_1, 
-                            help='Set the number of non trauma pathway treatment cubicles')
+    st.markdown("# Parameters")
 
-    st.markdown('## Trauma Pathway') 
-    # proportion of patients triaged as trauma
-    trauma_p = st.slider('Probability trauma patient', 0.0, 1.0, 
-                          md.DEFAULT_PROB_TRAUMA, 0.01)
+    # Number of rooms
+    st.markdown("## Capacity constraints")
 
-    trauma_mean = st.slider('Mean treatment time', 0.0, 100.0, 
-                          md.DEFAULT_TRAUMA_TREAT_MEAN, 1.0)
+    triage_bays = st.slider(
+        "Triage bays", 1, 5, md.DEFAULT_N_TRIAGE, help="Number of triage bays")
 
-    trauma_var = st.slider('Variance of treatment time', 0.0, 10.0, 
-                             md.DEFAULT_TRAUMA_TREAT_VAR, 0.5)
+    exam_rooms = st.slider(
+        "Exam rooms", 1, 5, md.DEFAULT_N_EXAM,
+        help="Number of exam rooms on the non-trauma pathway")
 
-    st.markdown('## Non-Trauma Pathway') 
+    treat_rooms = st.slider(
+        "Non-Trauma Treatment cubicles", 1, 5, md.DEFAULT_N_CUBICLES_1,
+        help="Number of treatment cubicles on the non-trauma pathway")
 
-    #examination mean
-    exam_mean = st.slider('Mean examination time', 0.0, 45.0, 
-                          md.DEFAULT_EXAM_MEAN, 1.0)
-    
-    exam_var = st.slider('Variance of examination time', 0.0, 15.0, 
-                          md.DEFAULT_EXAM_VAR, 0.5)
+    # Trauma pathway
+    st.markdown("## Trauma Pathway")
 
-    # proportion of non-trauma patients that require treatment
-    nontrauma_treat = st.slider('Probability non-trauma treatment', 0.0, 1.0, 
-                          md.DEFAULT_NON_TRAUMA_TREAT_P)
+    trauma_p = st.slider(
+        "Probability trauma patient", 0.0, 1.0, md.DEFAULT_PROB_TRAUMA, 0.01,
+        help="Probability of patients being triaged as trauma")
 
-    nt_trauma_mean = st.slider('Mean treatment time', 0.0, 100.0, 
-                          md.DEFAULT_NON_TRAUMA_TREAT_MEAN, 1.0)
+    trauma_mean = st.slider(
+        "Mean treatment time", 0.0, 100.0, md.DEFAULT_TRAUMA_TREAT_MEAN, 1.0,
+        help="Mean treatment time on trauma pathway (min)")
 
-    nt_trauma_var = st.slider('Variance of treatment time', 0.0, 10.0, 
-                             md.DEFAULT_NON_TRAUMA_TREAT_VAR, 0.5)
-                            
-# put info in columns
+    trauma_var = st.slider(
+        "Variance of treatment time", 0.0, 10.0, md.DEFAULT_TRAUMA_TREAT_VAR,
+        0.5, help="Variance in treatment times on trauma pathway (min)")
+
+    # Non-trauma pathway
+    st.markdown("## Non-Trauma Pathway")
+
+    exam_mean = st.slider(
+        "Mean examination time", 0.0, 45.0, md.DEFAULT_EXAM_MEAN, 1.0,
+        help="Mean length of examination (min)")
+
+    exam_var = st.slider(
+        "Variance of examination time", 0.0, 15.0, md.DEFAULT_EXAM_VAR, 0.5,
+        help="Variance in length of examination (min)")
+
+    nontrauma_treat = st.slider(
+        "Probability non-trauma treatment", 0.0, 1.0,
+        md.DEFAULT_NON_TRAUMA_TREAT_P,
+        help="Probability of non-trauma patients requiring treatment")
+
+    nt_trauma_mean = st.slider(
+        "Mean treatment time", 0.0, 100.0, md.DEFAULT_NON_TRAUMA_TREAT_MEAN,
+        1.0, help="Mean length of non-trauma treatment (min)")
+
+    nt_trauma_var = st.slider(
+        "Variance of treatment time", 0.0, 10.0,
+        md.DEFAULT_NON_TRAUMA_TREAT_VAR, 0.5,
+        help="Variance in length of non-trauma treatment (min)")
+
+# Describe treatment process and daily arrival patterns in columns
 col1, col2 = st.columns(2)
-with col1.expander('Treatment process', expanded=False):
-    st.image('img/process_flow_img.png')
+with col1.expander("Treatment process", expanded=False):
+    st.image("img/process_flow_img.png")
     st.markdown(INFO_3)
     st.markdown(INFO_4)
-
-with col2.expander('Daily Arrival Pattern', expanded=False):
+with col2.expander("Daily Arrival Pattern", expanded=False):
     st.plotly_chart(get_arrival_chart(), use_container_width=True)
 
-st.markdown(INFO_1a)
+# Suggestion to vary parameters
+st.markdown(INFO_2)
 
-# set up scenario    
+# Set up scenario
 args = md.Scenario()
 args.n_triage = triage_bays
 args.n_exam = exam_rooms
@@ -115,24 +116,26 @@ args.prob_trauma = trauma_p
 args.exam_mean = exam_mean
 args.exam_var = exam_var
 
-# MODFICATION - moved from the sidebar.
-replications = st.number_input("Multiple runs", value=10, placeholder="Enter no. replications to run...")
+# Set the number of replications
+replications = st.number_input(
+    "Multiple runs", value=10, placeholder="Enter no. replications to run...",
+    help="Number of replications to run")
 
-if st.button('Simulate treatment centre'):
+if st.button("Simulate treatment centre"):
     # Get results
-    with st.spinner('Simulating the treatment centre...'):
-        results = md.multiple_replications(args, n_reps = replications)
-        
-    st.success('Done!')
-    
+    with st.spinner("Simulating the treatment centre..."):
+        results = md.multiple_replications(args, n_reps=replications)
+    st.success("Done!")
+
+    # Display table of results
     col1, col2 = st.columns(2)
-    with col1.expander('Tabular results', expanded=True):
+    with col1.expander("Tabular results", expanded=True):
         summary_series = results.mean().round(1)
-        summary_series.name = 'Mean'
+        summary_series.name = "Mean"
         st.table(summary_series)
 
-    with col2.expander('MORE Plot', expanded=True):
-        more_fig = more_plotly(results['09_throughput'].to_numpy(), 
-                               x_label='Average Daily Throughput')     
-        st.plotly_chart(more_fig, use_container_width=True)                     
-
+    # Display MORE plot
+    with col2.expander("MORE Plot", expanded=True):
+        more_fig = more_plotly(results["09_throughput"].to_numpy(),
+                               x_label="Average Daily Throughput")
+        st.plotly_chart(more_fig, use_container_width=True)
